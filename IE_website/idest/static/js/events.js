2ed27477e347688e750111f44db3902884ad754c
oscar eduardo corona sanchez <os.cosa@hotmail.com>
2023-01-11 21:40:33 -0800

    'fonts'

--- IE_website/idest/static/js/events.js
+++ IE_website/idest/static/js/events.js
@@ -1,6 +1,22 @@
+var font = 0
 
 $('h1').on('click',function(){
     //$('.collapse').collapse():
     console.log('hi')
 })
-
+$('.changefont').on('click',function(){
+    
+    if ((font%2) === 0){
+        
+        console.log('cou');
+        $('p').css({"font-family":'COURIERPRIME-REGULAR'});
+            
+    }else if (font%2 !== 0){
+        
+        console.log('com');
+        $('p').css({"font-family":'COMPAGNON-ROMAN','font-weight':"bold"});
+            
+        
+    }
+    font++
+})
