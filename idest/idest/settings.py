6e5751361f78b59deea2b16e425055adad32eff6
oscar eduardo corona sanchez <os.cosa@hotmail.com>
2023-08-18 04:27:43 -0700

    'host2'

--- idest/idest/settings.py
+++ idest/idest/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('WEBSITE_NAME')]
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS=[os.environ.get('WEBSITE_NAME')]
 
 
