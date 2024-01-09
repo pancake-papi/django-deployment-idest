66bcc1d104de5316302ffae61adada199c6fd52b
oscar eduardo corona sanchez <os.cosa@hotmail.com>
2024-01-08 17:41:37 -0800

    'site2'

--- idest/idest/settings.py
+++ idest/idest/settings.py
@@ -38,7 +38,7 @@ LOGIN_REDIRECT_URL = "/"
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['idest.up.railway.app',
                  "127.0.0.1",
