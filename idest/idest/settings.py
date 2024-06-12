8a983b1790a3ad201518b15cbf8642bd015ebae3
oscar eduardo corona sanchez <os.cosa@hotmail.com>
2024-06-12 02:43:12 -0700

    'cs3'

--- idest/idest/settings.py
+++ idest/idest/settings.py
@@ -38,7 +38,7 @@ LOGIN_REDIRECT_URL = "/"
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #os.environ.get('DEBUG')
 
